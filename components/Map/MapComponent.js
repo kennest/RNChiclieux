2ae22a83a832370a4e70cbf972a794db8d4febbc
WeDevAgri <wedevagri@MacBook-Pro-de-Macbook.local>
2019-10-16 22:59:31 +0000

    16/10/2019

--- components/Map/MapComponent.js
+++ components/Map/MapComponent.js
@@ -1,6 +1,8 @@
 import React,{Component} from "react";
-import {View,StyleSheet,Dimensions} from "react-native";
+import {View, StyleSheet, Dimensions, TextInput} from "react-native";
 import MapView, {MAP_TYPES} from "react-native-maps";
+import {observer} from "mobx-react";
+import SearchBar from 'react-native-search-bar';
 const {width, height} = Dimensions.get('window');
 
 const ASPECT_RATIO = width / height;
@@ -10,6 +12,7 @@ const LONGITUDE = -3.955779;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
+@observer
 class MapComponent extends Component {
 
     constructor(props) {
@@ -21,7 +24,6 @@ class MapComponent extends Component {
                 latitudeDelta: LATITUDE_DELTA,
                 longitudeDelta: LONGITUDE_DELTA,
             },
-            points: [],
             location: {
                 latitude: 0.0,
                 longitude: 0.0,
@@ -35,6 +37,15 @@ class MapComponent extends Component {
         };
         return (
             <View style={styles.container}>
+                <View style={styles.searchBar}>
+                    <SearchBar
+                        ref="searchBar"
+                        placeholder="Search"
+                        onChangeText={()=>{}}
+                        onSearchButtonPress={this.refs.searchBar.unFocus}
+                        onCancelButtonPress={()=>{}}
+                    />
+                </View>
                 <MapView
                     style={styles.map}
                     mapType={MAP_TYPES.HYBRID}
@@ -58,6 +69,10 @@ const styles = StyleSheet.create({
     map: {
         ...StyleSheet.absoluteFillObject,
     },
+    searchBar:{
+        position:'absolute',
+        margin:10
+    }
 });
 
 export default MapComponent;
